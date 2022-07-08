0a92eab5833e255d114f24952eaf14387ea10fa8
Romu Tiwari <romtiwar@publicisgroupe.net>
2022-07-08 14:09:10 +0530

    Swagger changes

--- src/main/java/com/example/angular/springbootangularkubernetes/config/SwaggerConfig.java
+++ src/main/java/com/example/angular/springbootangularkubernetes/config/SwaggerConfig.java
@@ -4,10 +4,8 @@ package com.example.angular.springbootangularkubernetes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,17 +17,12 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.angular.springbootangularkubernetes"))
-				.paths(PathSelectors.any())
-				.build()
-				.apiInfo(apiInfo());
+	             .select()
+	              .apis(RequestHandlerSelectors.any())
+	               .paths(PathSelectors.regex("/api.*"))
+	               .build();
 	}
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("HR Management System - Backend")
-				.description("Organize HR Works Here")
-				.termsOfServiceUrl("localhost").version("1.0").build();
-	}
+	
 
 	
 
